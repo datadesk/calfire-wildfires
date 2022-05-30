4f843de23aa9a1ff208208c9d38a5caea32ad6da
palewire <b@palewi.re>
2022-05-29 21:12:11 -0700

    Throw in docs too

---
+++ docs/index.md
@@ -0,0 +1,72 @@
+```{include} _templates/nav.html
+```
+
+# calfire-wildfires
+
+Download wildfires data from CalFire
+
+```{contents} Table of contents
+:local:
+:depth: 2
+```
+
+## Installation
+
+```sh
+pipenv install calfire-wildfires
+```
+
+## Command-line usage
+
+```sh
+Usage: calfirewildfires [OPTIONS] COMMAND [ARGS]...
+
+    A command-line interface for downloading wildfires data from CalFire.
+    Returns GeoJSON.
+
+Options:
+    --help  Show this message and exit.
+
+Commands:
+    active-fires    The latest active fires
+    all-fires       Fires year-to-date, both active and contained
+```
+
+From the shell: 
+
+```sh
+calfirewildfires all-fires
+calfirewildfires active-fires
+```
+
+## Python usage
+
+Import the library. 
+
+```python
+import calfire_wildfires
+data = calfire_wildfires.get_all_fires()
+data = calfire_wildfires.get_active_fires()
+```
+
+## Contributing
+
+Install dependencies for development.
+
+```sh
+pipenv install --dev
+```
+
+Run tests.
+
+```sh
+pipenv run python test.py
+```
+
+## Developing the CLI
+
+The command-line interface is implemented using Click and setuptools. To install it locally for development inside your virtual environment, run the following installation command, as [prescribed by the Click documentation](https://click.palletsprojects.com/en/7.x/setuptools/#setuptools-integration).
+
+```sh
+pipenv run pip install --editable .
+```
--- setup.py
+++ setup.py
@@ -7,15 +7,45 @@ def read(fname):
         return f.read()
 
 
+def version_scheme(version):
+    """
+    Version scheme hack for setuptools_scm.
+
+    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
+
+    If that issue is resolved, this method can be removed.
+    """
+    import time
+
+    from setuptools_scm.version import guess_next_version
+
+    if version.exact:
+        return version.format_with("{tag}")
+    else:
+        _super_value = version.format_next_version(guess_next_version)
+        now = int(time.time())
+        return _super_value + str(now)
+
+
+def local_version(version):
+    """
+    Local version scheme hack for setuptools_scm.
+
+    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
+
+    If that issue is resolved, this method can be removed.
+    """
+    return ""
+
+
 setup(
     name='calfire-wildfires',
-    version='0.0.4',
     description="Download wildfires data from CalFire",
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
-    author='Los Angeles Times Data and Graphics Department',
-    author_email='datagraphics@caltimes.com',
-    url='http://www.github.com/palewire/calfire-wildfires',
+    author='Ben Welsh',
+    author_email='b@palewi.re',
+    url='http://www.github.com/datadesk/calfire-wildfires',
     license="MIT",
     packages=("calfire_wildfires",),
     install_requires=[
@@ -26,18 +56,19 @@ setup(
         [console_scripts]
         calfirewildfires=calfire_wildfires.cli:cmd
     """,
+    use_scm_version={"version_scheme": version_scheme, "local_scheme": local_version},
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: MIT License',
     ],
     project_urls={
-        'Maintainer': 'https://github.com/palewire',
-        'Source': 'https://github.com/palewire/calfire-wildfires',
-        'Tracker': 'https://github.com/palewire/calfire-wildfires/issues'
+        'Maintainer': 'https://github.com/datadesk',
+        'Source': 'https://github.com/datadesk/calfire-wildfires',
+        'Tracker': 'https://github.com/datadesk/calfire-wildfires/issues'
     },
 )
