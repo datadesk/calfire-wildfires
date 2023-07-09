1049e585a7b5ffe07fec9d3325c156da5bf7fe26
Ben Welsh <b@palewi.re>
2023-07-09 19:37:30 +0000

    Update stuff

--- calfire_wildfires/__init__.py
+++ calfire_wildfires/__init__.py
@@ -12,13 +12,14 @@ def get_active_fires():
 
     print("Requesting active Fires")
     # Request data
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36', # noqa
-               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', # noqa
-               'Accept-Language': 'en-US,en;q=0.9'
-               }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",  # noqa
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",  # noqa
+        "Accept-Language": "en-US,en;q=0.9",
+    }
     r = requests.get(
         "https://www.fire.ca.gov/umbraco/api/IncidentApi/GeoJsonList?inactive=false",
-        headers=headers
+        headers=headers,
     )
     if r.status_code != 200:
         raise Exception(f"Request for data failed with {r.status_code} status code")
