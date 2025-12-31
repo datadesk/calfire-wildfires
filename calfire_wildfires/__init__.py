314ae7406c5507fc848bf33f6d026c68ebbaae15
Vanessa Martinez <11972166+vnessamartinez@users.noreply.github.com>
2025-12-30 19:31:11 -0800

    updated headers

--- calfire_wildfires/__init__.py
+++ calfire_wildfires/__init__.py
@@ -11,10 +11,12 @@ def get_active_fires():
     """
     # Request data
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",  # noqa
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",  # noqa
+        "Accept": "application/json",
+        "Accept-Language": "en-US, en;q=0.9"
     }
     url = "https://www.fire.ca.gov/umbraco/api/IncidentApi/GeoJsonList?inactive=false"
-    r = requests.get(url, headers=headers)
+    r = requests.get(url)
 
     # Make sure get a good response
     try:
@@ -33,10 +35,12 @@ def get_all_fires():
     """
     # Request data
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",  # noqa
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",  # noqa
+        "Accept": "application/json",
+        "Accept-Language": "en-US, en;q=0.9"
     }
     url = "https://www.fire.ca.gov/umbraco/api/IncidentApi/GeoJsonList"
-    r = requests.get(url, headers=headers)
+    r = requests.get(url)
 
     # Make sure the response works
     try:
